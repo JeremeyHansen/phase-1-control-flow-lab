bde76ab35efe8c156ed47d27cd07ede0368330e7
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-08 22:53:07 -0400

    commit tree 97b1d46563bf9a31d51054f79ef21e99081d3685

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  var someValue = 199;
+  let result
+  if (someValue < 400) {
+    result = 'This one is on me!';
+  }
+  return result
+}
+
+function scuberGreetingForFeet(someValue) {
+  var someValue = 1500
+  let result
+  if (someValue > 0) {
+    if (someValue < 2000) {
+    result = 'That will be twenty bucks.';
+  }
+  }
+  return result
 }
 
 function ternaryCheckCity(){
